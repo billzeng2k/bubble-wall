9b544718c5ee18bd00b0d08a714e05d5de75c6c9
Bill Zeng <billzeng@Bills-MacBook-Pro-2.local>
2019-08-24 00:01:35 -0400

    bubbles pressable

--- src/components/bubble.js
+++ src/components/bubble.js
@@ -3,9 +3,14 @@ import './tube.css';
 import bubble from './bubble.png'
 
 class Bubble extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {on: false}
+    }
+
     render() {
         return (
-            <img src={bubble} className='bubble'/>
+            <img src={bubble} className='bubble' style={{opacity: this.state.on ? 1 : 0.3}} onClick={() => this.setState({on: !this.state.on})} onDragStart={(e) => {e.preventDefault()}}/>
         );
     }
 }
