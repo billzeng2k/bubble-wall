32deacc48e6906a30b18c8005c9920ccc501282e
lucaszw <lucas.zeer@gmail.com>
2019-08-25 07:23:31 -0400

    added LED support + improved performance

--- hardware/solenoids/solenoids.ino
+++ hardware/solenoids/solenoids.ino
@@ -1,34 +1,77 @@
-#define ledPin 2
-
 #include <SoftwareSerial.h>
+#include <FastLED.h>
 
-SoftwareSerial mySerial(12, 11); // RX, TX
+#define LED_PIN     9
+#define NUM_LEDS    20
+CRGB leds[NUM_LEDS];
 
-int pins[9] = {2, 3, 10, 9, 4, 5, 6, 7, 8};
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+int pins[9] = {12, 6, 8, 13, 5, 4, 7, 2, 3};
 
 String input;
-String solenoid;
-String state;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  for (int i=1;i<9; i++) {
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  
+  for (int i=0;i<9; i++) {
     pinMode(pins[i], OUTPUT);
     digitalWrite(pins[i], HIGH);
   }
   mySerial.begin(9600); // Default communication rate of the Bluetooth module
   Serial.begin(9600);
+
+  for (int i=0;i<20;i++)
+    leds[i] = CRGB(0,0,0);
+  
+  FastLED.show();
+}
+
+int k = millis();
+void loop() {
+  bool updated = updateSolenoids();
+  if (updated == true) {
+    k = millis();
+    return;
+  }
+
+//  if (millis() - k >= 3000) {
+//    openSolenoid(pins[0]);
+//    delay(1000);
+//    closeAll();
+//    k=millis();
+//  }
+//  for (int i=0;i<9; i++) {
+//    openSolenoid(pins[i]);
+//    turnOnRGB(i);
+//    delay(2000);
+//  }
+//  for (int i=0;i<20;i++)
+//    leds[i] = CRGB(0,0,0);
+  
+   //updateSolenoids();
+//   for (int i=0;i<7;i++) {
+//    for (int ii=0;ii<10;ii++) {
+//        openSolenoid(pins[i]);
+//        delay(50);
+//        openSolenoid(pins[i+1]);
+//        delay(50);
+//        openSolenoid(pins[i+2]);
+//        delay(50);
+//      } 
+//   }
+  
 }
 
 void closeAll() {
-  for (int i=1;i<9; i++) {
+  for (int i=0;i<9; i++) {
     digitalWrite(pins[i], HIGH);
   }
 }
 
-void printState(String solenoid, String state) {
-  Serial.println("SOLENOID: "+solenoid);
-  Serial.println("STATE: "+state);
+void printState(int solenoid, int state) {
+  Serial.println("SOLENOID: " + String(solenoid));
+  Serial.println("STATE: " + String(state));
 }
 
 int countInstanceOfChar(String string, char c) {
@@ -57,22 +100,21 @@ String getValue(String data, char separator, int index)
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-void writePins(String input) {
-    solenoid = input.substring(0, 1);
-    state = input.substring(2,3);
-    if (solenoid == "0") {
-      for (int i=1;i<9; i++) {
-        pinMode(pins[i], OUTPUT);
-        digitalWrite(pins[i], HIGH);
-      }
-    } else {
-       if (state == "0" && solenoid != "2") {
-      digitalWrite(2, 1);
-    } else {
-      digitalWrite(2, 0);
+void writePins(String data) {
+    int count = countInstanceOfChar(data, ',');
+    // Open input, close all others, and turn on lights
+    int solenoid = getValue(data, ',', 0).toInt();
+    int state = getValue(data, ',', 1).toInt();
+    if (count > 2){
+      int r = getValue(data, ',', 2).toInt();
+      int g = getValue(data, ',', 3).toInt();
+      int b = getValue(data, ',', 4).toInt();
+      leds[2*solenoid+1] = CRGB(r, g, b);
+      leds[2*solenoid] = CRGB(r, g, b); 
+      FastLED.show();
     }
-    digitalWrite(solenoid.toInt(), state.toInt());
-    }
+    if (state == 1) openSolenoid(pins[solenoid]);
+    if (state == 0) closeAll();
     printState(solenoid, state);
 }
 
@@ -82,30 +124,46 @@ void turnOnSingle() {
     writePins(input);
 }
 
-void turnOnMany() {
-    String s = mySerial.readString(); // Reads the data from the serial port
+void turnOnMany(String s) {
+    Serial.println(s);
     int count = countInstanceOfChar(s, '&');
     for (int i = 0;i<count+1;i++) {
-      closeAll();
       input = getValue(s, '&', i);
       writePins(input);
       delay(200);
     }
-    closeAll();
+    turnOffLEDS();
+    
+//    openEscape();
 }
-void updateSolenoids() {
+
+void turnOffLEDS() {
+  for (int i=0;i<20;i++)
+    leds[i] = CRGB(0,0,0);
+  FastLED.show();
+}
+
+void openEscape() {
+  for (int i=0;i<9; i++) {
+    digitalWrite(pins[i], HIGH);
+  }
+  digitalWrite(pins[0], LOW);
+}
+
+bool updateSolenoids() {
   if(mySerial.available() > 0){ // Checks whether data is comming from the serial port
-    turnOnMany();
+    turnOnMany(mySerial.readString());
+    return true;
   }
 
   if (Serial.available() > 0) {
-    input = Serial.readString(); // Reads the data from the serial port
-    solenoid = input.substring(0, 1);
-    state = input.substring(2,3);
-    digitalWrite(solenoid.toInt(), state.toInt());
-    printState(solenoid, state);
+    turnOnMany(Serial.readString());
+    return true;
   }
+
+  return false;
 }
+
 void openSolenoid(int pin) {
   for (int i=0;i<9;i++) {
     if (pins[i] == pin) {
@@ -116,17 +174,37 @@ void openSolenoid(int pin) {
   }
 }
 
+void turnOnRGB(int col) {
+     
+    int r = 0;
+    int g = 0;
+    int b = 0;
 
-void loop() {
-   //updateSolenoids();
-   for (int i=0;i<7;i++) {
-    for (int ii=0;ii<10;ii++) {
-        openSolenoid(pins[i]);
-        delay(50);
-        openSolenoid(pins[i+1]);
-        delay(50);
-        openSolenoid(pins[i+2]);
-        delay(50);
-      } 
-   }
+    if ((col % 3) == 0)
+      r = 255;
+
+    if ((col % 3) == 1)
+      g = 255;
+
+    if ((col % 3) == 2)
+      b = 255;
+
+    if (col <= 5)
+      leds[2*col+3] = CRGB(r, g, b);
+
+    if (col > 5)
+      leds[2*col+2] = CRGB(r, g, b);
+
+    FastLED.show();
+}
+
+void loopRandomColors() {
+  for (int i=0;i<20;i++) {
+    int r = random(0, 10);
+    int g = random(0, 10);
+    int b = random(0, 10);
+    leds[i] = CRGB(r, g, b);
+    FastLED.show();
+    delay(500); 
+  }
 }
